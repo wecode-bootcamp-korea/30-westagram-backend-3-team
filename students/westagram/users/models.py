c196255633e143c19b6960fcc00c1ce58053a6cd
kimkaramhwi <kimkrh@naver.com>
2022-02-17 18:52:45 +0900

    Add: User Model

--- students/westagram/users/models.py
+++ students/westagram/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     username   = models.CharField(max_length=30)
     email      = models.EmailField(max_length=300, unique=True)
     password   = models.CharField(max_length=200)
-    contact    = models.CharField(max_length=30, unique=True)
+    contact    = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
