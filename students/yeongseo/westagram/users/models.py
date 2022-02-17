1ce4f88e705fcaffae596e2a7c997fb29a728189
Park YengSeo <1park4170@gmail.com>
2022-02-17 17:51:13 +0900

    name -> username added unique=True to email password max_length modified added created_at , updated_at column

--- students/yeongseo/westagram/users/models.py
+++ students/yeongseo/westagram/users/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    name             = models.CharField(max_length=20)
-    email             = models.EmailField(max_length=40)
-    password        = models.CharField(max_length=40)
+    username        = models.CharField(max_length=30)
+    email             = models.EmailField(max_length=40, unique=True)
+    password        = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=16) 
-    #+82등 국가번호 붙는 경우 감안하여 char로 필드 지정
+    created_at       = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
     class Meta:
         db_table = 'users'
