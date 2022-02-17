21dd68e065a9f623ed002729643b8ba7f9876979
kimkaramhwi <kimkrh@naver.com>
2022-02-17 17:32:42 +0900

    Add: User Model

--- students/westagram/users/models.py
+++ students/westagram/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-from django.db import models
-
-class User(models.Model):
-    name         = models.CharField(max_length=30)
-    email        = models.CharField(max_length=100)
-    password     = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=50)
+class user(models.Model):
+    username   = models.CharField(max_length=30)
+    email      = models.EmailField(max_length=300, unique=True)
+    password   = models.CharField(max_length=200)
+    contact    = models.CharField(max_length=30, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
