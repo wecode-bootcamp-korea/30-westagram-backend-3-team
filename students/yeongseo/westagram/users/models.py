94731f3cf2e027f53e05db1c2b17e83e31aa965b
Park YengSeo <1park4170@gmail.com>
2022-02-17 18:13:47 +0900

    aligned code space between class Users / class Meta

--- students/yeongseo/westagram/users/models.py
+++ students/yeongseo/westagram/users/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Users(models.Model):
     username        = models.CharField(max_length=30)
-    email             = models.EmailField(max_length=40, unique=True)
+    email           = models.EmailField(max_length=40, unique=True)
     password        = models.CharField(max_length=150)
-    phone_number = models.CharField(max_length=16) 
-    created_at       = models.DateTimeField(auto_now_add=True)
+    phone_number    = models.CharField(max_length=16) 
+    created_at      = models.DateTimeField(auto_now_add=True)
     updated_at      = models.DateTimeField(auto_now=True)
+
     class Meta:
         db_table = 'users'
