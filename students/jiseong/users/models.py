f6aa78af27763b4f30514a1b32581bcd1b6410b8
jayce-kim1123 <kimjisung777@gmail.com>
2022-02-17 20:36:31 +0900

    fix: edit models.py

--- students/jiseong/users/models.py
+++ students/jiseong/users/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.forms import PasswordInput
-
-#이름 이메일 비번 연락처
 
 class User(models.Model):
     last_name    = models.CharField(max_length=45)
@@ -9,7 +6,8 @@ class User(models.Model):
     email        = models.CharField(max_length=45, unique=True)
     password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=45)
-    create_at    = models.DateTimeField(auto_now_add=False)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table='users'
