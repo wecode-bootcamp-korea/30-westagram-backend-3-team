0c20ddb30490780948eda89d234597554e0b095c
Park YengSeo <1park4170@gmail.com>
2022-02-17 17:09:08 +0900

    Modify : User model

--- students/yeongseo/westagram/users/models.py
+++ students/yeongseo/westagram/users/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=40)
-    password = models.CharField(max_length=40)
-    phone_number = models.CharField(max_length=16) #+82등 국가번호 붙는 경우 감안하여 char로 필드 지정
+    name             = models.CharField(max_length=20)
+    email             = models.EmailField(max_length=40)
+    password        = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=16) 
+    #+82등 국가번호 붙는 경우 감안하여 char로 필드 지정
     class Meta:
         db_table = 'users'
