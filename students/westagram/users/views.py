88be25864ba1b9585bc77b7e75197a60a280122c
kimkaramhwi <kimkrh@naver.com>
2022-02-21 17:14:24 +0900

    Modify: bcrypt

--- students/westagram/users/views.py
+++ students/westagram/users/views.py
@@ -15,7 +15,7 @@ class SignUpView(View):
             email            = data.get('email')
             password         = data.get('password')
             contact          = data.get('contact', "")
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            hashed_password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
             if validate_email(email) == False:
                 return JsonResponse({'messasge':'INVALID EMAIL'}, status=400) 
